79aaea50238b62927526107f1b4732e1f19bf5e8
Nishu singh <nishu.frontend@gmail.com>
2026-03-06 20:30:09 +0530

    add DropDownMenu component with styling

--- App.jsx
+++ App.jsx
@@ -1,12 +1,16 @@
-import './App.css';
-import Header from './components/Header';
-import SearchBar from './components/SearchBar';
+import "./App.css";
+import DropDownMenu from "./components/DropDownMenu";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 export default function App() {
   return (
     <>
-    <Header/>
-    <SearchBar/> 
+      <Header />
+      <div className="searchbar-and-dropmenu-container">
+        <SearchBar />
+        <DropDownMenu />
+      </div>
     </>
-  )
+  );
 }
---
+++ components/DropDownMenu.jsx
@@ -0,0 +1,13 @@
+export default function DropDownMenu() {
+  return (
+<select id="region">
+<option value="">Filter by Region</option>
+<option value="asia">Asia</option>
+<option value="europe">Europe</option>
+<option value="north-america">North America</option>
+<option value="south-america">South America</option>
+<option value="africa">Africa</option>
+<option value="australia">Australia</option>
+</select>
+  )
+}
