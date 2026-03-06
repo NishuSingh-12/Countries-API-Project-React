0e75f63759dce97d853e3f0b1bc2f8e883dcafd9
Nishu singh <nishu.frontend@gmail.com>
2026-03-06 20:24:45 +0530

    add SearchBar component with styling

---
+++ components/SearchBar.jsx
@@ -0,0 +1,8 @@
+export default function SearchBar({setQuery}) {
+  return (
+    <div className="SearchBar-container">
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input onChange={(e)=>setQuery(e.target.value)} type="text" placeholder="Search for a country..."/>
+    </div>
+  )
+}
