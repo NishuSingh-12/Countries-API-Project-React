05bb29dd282c2f2372aaadee0be369c21a880517
Nishu singh <nishu.frontend@gmail.com>
2026-03-06 20:21:33 +0530

    add Header component with styling

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+   <header className="header">
+    <div>
+      <h2>Where in the world?</h2>
+      <p><span><i className="fa-regular fa-moon"></i></span>Dark</p>
+    </div>
+   </header>
+  )
+}
