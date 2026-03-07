6b06528ed9e3238fb2e7e85ed3946bc72be9c457
Nishu singh <nishu.frontend@gmail.com>
2026-03-07 21:29:31 +0530

    In Error component show error message using useRouteError hook

--- components/Error.jsx
+++ components/Error.jsx
@@ -1,3 +1,7 @@
+import { useRouteError } from "react-router-dom";
+
 export default function Error() {
-  return <h1 className="error-msg">Page not found..</h1>;
+  const error = useRouteError();
+
+  return <h1 className="error-msg">Page not found...{error.status}</h1>;
 }
