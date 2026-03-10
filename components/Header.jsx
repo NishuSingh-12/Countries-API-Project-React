c1376b4c4e90173f42087478c522f8a120ba75ec
Nishu singh <nishu.frontend@gmail.com>
2026-03-10 10:42:00 +0530

    add dark/light mode functionality and fix css

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,10 +4,11 @@ import CountryCardList from "./CountryCardList";
 export default function Header() {
   return (
    <header className="header">
-    
     <div>
       <h2>Where in the world?</h2>
-      <p><span><i className="fa-regular fa-moon"></i></span>Dark</p>
+      <p onClick={()=>{
+        document.body.classList.toggle('dark')
+      }}><span><i className="fa-regular fa-moon"></i></span>Dark</p>
     </div>
    </header>
   )
