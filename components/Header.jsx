739ab25168957abdf8115352b9eb54cebe54948b
Nishu singh <nishu.frontend@gmail.com>
2026-03-10 12:45:30 +0530

    save dark/light mode in localStorage

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,24 @@
 
+import { useState } from "react";
 import CountryCardList from "./CountryCardList";
 
 export default function Header() {
+  const [isDark,setIsDark]=useState(JSON.parse(localStorage.getItem('isDarkMode')))
+
+  if(isDark){
+     document.body.classList.add('dark')
+  }else{
+     document.body.classList.remove('dark')
+  }
+  
   return (
    <header className="header">
     <div>
       <h2>Where in the world?</h2>
       <p onClick={()=>{
-        document.body.classList.toggle('dark')
-      }}><span><i className="fa-regular fa-moon"></i></span>Dark</p>
+        setIsDark(!isDark)
+        localStorage.setItem('isDarkMode', !isDark)
+      }}><span><i className={`fa-solid fa-${isDark ? "sun" : "moon"}`}></i></span>{isDark ? "Light" : "Dark"} Mode</p>
     </div>
    </header>
   )
--- components/Home.jsx
+++ components/Home.jsx
@@ -7,12 +7,12 @@ import SearchBar from "./SearchBar";
 export default function Home() {
     const [query,setQuery] = useState("")
   return (
-    <>
+    <main>
           <div className="searchbar-and-dropmenu-container">
             <SearchBar setQuery={setQuery}/>
             <DropDownMenu />
           </div>
         <CountryCardList query={query}/>
-    </>
+    </main>
   )
 }
